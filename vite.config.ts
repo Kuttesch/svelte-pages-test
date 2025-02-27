84cdbaeddf33809450f012e8e31f281d8e7643b7
Kuttesch <moritzkuttesch@outlook.de>
2025-02-27 21:55:47 +0100

    hopefully deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/svelte-pages-test'
+  base: '/svelte-pages-test',
+  build: {
+    outDir: 'docs', // Output to the 'docs' folder
+  },
 })
